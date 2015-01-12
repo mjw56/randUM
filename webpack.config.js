4fea0872f4c75e1f5f7b54a4984f8f9dab6e37e6
Mike Wilcox <mwilcox56@gmail.com>
2015-01-11 20:27:03 -0500

    fix indendation

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-  'webpack-dev-server/client?http://0.0.0.0:3000',
-  'webpack/hot/only-dev-server',
-  './src/js/app'
+    'webpack-dev-server/client?http://0.0.0.0:3000',
+    'webpack/hot/only-dev-server',
+    './src/js/app'
   ],
   output: {
     path: __dirname,
@@ -13,16 +13,16 @@ module.exports = {
     publicPath: '/dist/'
   },
   plugins: [
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoErrorsPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
   ],
   resolve: {
     extensions: ['', '.js', '.json']
   },
   module: {
     loaders: [
-    {test: /\.json$/, loader: 'json-loader'},
-    { test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot', '6to5-loader'] }
+      {test: /\.json$/, loader: 'json-loader'},
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot', '6to5-loader'] }
     ]
   }
 };
