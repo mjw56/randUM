024e8c08c4b55e0e365bc5d50bd46730fd9fd2f9
Mike Wilcox <mwilcox56@gmail.com>
2015-01-14 21:45:18 -0500

    get audio and render cassette

--- src/js/components/home.react.js
+++ src/js/components/home.react.js
@@ -1,15 +1,29 @@
 import * as React from 'react';
+import $ from 'jquery';
 import style from '../utils/styles';
 import useSheet from 'react-jss';
+import Cassette from 'react-cassette-player';
+import { Promise } from 'es6-promise';
 
 export default React.createClass({
+
+  getInitialState: function() {
+    return { audio: '' };
+  },
+
+  componentDidMount: function() {
+    Promise.resolve($.ajax('http://localhost:3001/api/track'))
+      .then((track) => {
+        this.setState( { audio: track });
+      });
+  },
 
   mixins: [useSheet(style.home)],
 
   render() {
     return (
       <div>
-        <h1 className={this.sheet.classes.h1}>RxJS Lab</h1>
+        { this.state.audio ? <Cassette src={this.state.audio} /> : <div /> }
       </div>
     );
   }
